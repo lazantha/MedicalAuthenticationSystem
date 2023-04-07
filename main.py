9815f53297130926562fc17b0f9257cd798e7257
lazantha <sglasanthapradeep@gmail.com>
2023-04-07 09:59:15 +0530

    updated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect,Flask
 app=Flask(__name__)
+app.config['SECRET_KEY']="kEY"
 
 #error handling 404
 @app.errorhandler(404)
@@ -8,14 +9,26 @@ def page_not_found(e):
 #error handling 500
 def internal_server_error(e):
   return render_template('errorhandler/500.html'), 500
+
+
+
+
 #home page
 @app.route('/')
 def index():
     return render_template('index.html')
-#user login Page
+
+
+    #user login Page
 @app.route('/userlog')
 def userlog():
-    return render_template('login/user.html')
+    # if request.method=='POST':
+    #     user_name=request.form['user_name']
+    #     password=request.form['password']
+
+
+     return render_template('login/user.html')
+
 
 #admin login page
 @app.route('/adminlog')
@@ -30,26 +43,13 @@ def about():
 def contact():
     return render_template('contact.html')
 
-from wtforms import Form,StringField,validators,SelectField,TextAreaField,DateTimeField,DateField,RadioField,FileField,validators,SubmitField
-#FileField for picture
-class UserForm(Form):
-    userName=StringField('Name with Initials')
-    course=SelectField("Course",choices=['IT','Accountency','Management','Tourism'])
-    year=SelectField("Year",choices=['1','2','3','4'])
-    semeseter=SelectField("Semester",choices=['1','2'])
-    attempt=SelectField("Attemp",choices=['1','2','3','4'])
-    comment=TextAreaField("Signs and Symptoms Observed By Medical Officer")
-    start_date=DateField("Start Date")
-    end_date=DateField("End Date")
-    doc_name=StringField("Name of the Medical Officer")
-    type=RadioField("Medical By",choices=['Government','Private'])
-    med_pic=FileField("Upload picture of Medical Sheet")
-    date_issued=DateField("Enter Date")
-    submit=SubmitField("Save")
 
-#user main form 
-@app.route('/user',methods=['GET,POST'])
+@app.route('/user')
 def user():
-    form=UserForm()
-    return render_template('users/frmUser.html',form=form)
+    # form=UserForm()
+    # if form.method==['POST']:
+    #     if form.validate_on_submit():
+    #         name=form.userName.data
+
+    return render_template('users/frmUser.html')
 
